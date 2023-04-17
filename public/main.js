75ea0703210c222c9f9bc27c2f17fb8c5dcb936b
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-04-17 06:54:31 -0700

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -9,21 +9,28 @@ const firebaseConfig = {
   measurementId: 'string'
 }
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig)
+
+// Create a reference to the Realtime Database
 const database = firebase.database()
 
+// Listen to mouse movement and write data to the database
 document.addEventListener('mousemove', (event) => {
   database.ref('cursor').set({
     x: event.clientX,
     y: event.clientY
   })
+  // Log the mouse coordinates to the console
   console.log(`X: ${event.clientX}, Y: ${event.clientY}`)
 })
 
+// Create a dot (cursor) and add it to the HTML
 const cursor = document.createElement('div')
 cursor.classList.add('cursor')
 document.body.appendChild(cursor)
 
+// Retrieve the cursor position from the Realtime Database and update the dot's style
 database.ref('cursor').on('value', (snapshot) => {
   const position = snapshot.val()
   cursor.style.left = `${position.x}px`
